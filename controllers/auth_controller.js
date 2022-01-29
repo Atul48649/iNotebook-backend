f3d1e8d421194b4f08d555e88eb750dd6e0c6de6
Atul Kumar <atul48649@gmail.com>
2022-01-29 12:29:13 +0530

    add passport-jwt-authentication and created api's for auth

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 module.exports.validate = function (method) {
     switch (method) {
@@ -9,6 +10,12 @@ module.exports.validate = function (method) {
                 body('name', "Enter a valid name").isLength({ min: 3 }),
                 body('email', 'Enter a valid email').exists().isEmail(),
                 body('password', 'Password should contain atleast 5 characters').isLength({ min: 5 }),
+            ];
+        }
+        case 'login': {
+            return [
+                body('email', 'Enter a valid email').exists().isEmail(),
+                body('password', 'Password cannot be blank').exists().isLength({ min: 1 })
             ];
         }
     }
@@ -38,11 +45,16 @@ module.exports.createUser = function (req, res) {
                     password: securePassword
                 }, function (err, user) {
                     if (err) { console.log('Error: ', err); return; }
+                    const body = {
+                        _id: user._id,
+                        email: user.email
+                    }
                     return res.status(200).json({
                         message: 'User created successfully!',
                         data: {
                             name: user.name,
-                            email: user.email
+                            email: user.email,
+                            token: jwt.sign(user._id.toJSON(), 'iNotebook')
                         }
                     });
                 })
@@ -58,7 +70,70 @@ module.exports.createUser = function (req, res) {
     } catch (err) {
         console.log('Error: ', err);
         return res.status(500).json({
-            message: 'Some error occured'
+            message: 'Internal Server Error'
+        });
+    }
+}
+
+module.exports.login = function (req, res) {
+    try {
+        // throw error in res.json if any
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(422).json({ errors: errors.array() });
+            return;
+        }
+        User.findOne({ email: req.body.email }, async function (err, user) {
+            if (err) { console.log('Error: ', err); return; }
+            if (!user) {
+                return res.status(400).json({
+                    error: 'Please try to login with valid credentials!'
+                });
+            }
+            const comparePassword = await bcrypt.compare(req.body.password, user.password);
+            if (!comparePassword) {
+                return res.status(400).json({
+                    error: 'Please try to login with valid credentials!'
+                });
+            }
+            const body = {
+                _id: user._id,
+                email: user.email
+            }
+            return res.status(200).json({
+                message: 'Login successfully!',
+                data: {
+                    name: user.name,
+                    email: user.email,
+                    token: jwt.sign(user._id.toJSON(), 'iNotebook')
+                }
+            });
+        })
+    } catch (err) {
+        console.log('Error: ', err);
+        return res.status(500).json({
+            message: 'Internal Server Error'
+        });
+    }
+}
+
+module.exports.getUser = async function (req, res) {
+    const jwt_payload = jwt.decode(req.body.token);
+    const userId = jwt_payload;
+    try {
+        let user = await User.findById(userId).select('-password');
+
+        if(user){
+            return res.status(200).json(user);
+        }else{
+            return res.status(401).json({
+                error: 'Please authenticate using a valid token'
+            });
+        }
+    } catch (err) {
+        console.log('Error: ', err);
+        return res.status(500).json({
+            error: 'Internal Server Error'
         });
     }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/auth_controller');
 
 router.post('/createuser', authController.validate('createUser') ,authController.createUser)
+router.post('/login', authController.validate('login'), authController.login)
+router.post('/getuser', authController.getUser);
 
 module.exports = router;
