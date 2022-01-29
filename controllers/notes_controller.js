019d80e2dd0d7a73426d059255a6f1472aaf371a
Atul Kumar <atul48649@gmail.com>
2022-01-29 16:59:53 +0530

    add delete note

--- controllers/notes_controller.js
+++ controllers/notes_controller.js
@@ -13,55 +13,6 @@ module.exports.validate = function (method) {
     }
 }
 
-// module.exports.updateNote = function(req, res){
-//     // find the note to be updated and update the note
-//     Notes.findById(req.params.id, function(err, note){
-//         if (err) { console.log('Error in finding a note: ', err); return; }
-//         if(!note){
-//             return res.status(404).send("Not Found")
-//         }
-//         if(note.user !== req.user.id){
-//             return res.status(401).send('Not Allowed');
-//         }
-
-//     })
-// }
-
-module.exports.updateNote = async function (req, res) {
-    try {
-                // destructuring req.body
-        const { title, description, tag } = req.body;
-
-        // create a update object
-        const update = {};
-        if (title) { update.title = title }
-        if (description) { update.description = description }
-        if (tag) { update.tag = tag }
-
-        let note = await Note.findById(req.params.id);
-        if (!note) {
-            return res.status(404).send("Not Found");
-        }
-
-        // get user id through the payload
-        const jwt_payload = jwt.decode(req.body.token);
-        const userId = jwt_payload;
-
-        if (note.user == userId) {
-            let updateNote = await Note.findByIdAndUpdate(req.params.id, update);
-            return res.status(200).json({
-                message: 'Note updated successfully',
-                data: update
-            });
-        } else {
-            return res.status(401).send('Not Allowed');
-        }
-    } catch (err) {
-        console.log('Error ', err);
-        return res.status(500).json({ 'error': 'Internal Server Error' });
-    }
-}
-
 module.exports.createNote = function (req, res) {
     // throw error in res.json if any
     const errors = validationResult(req);
@@ -104,6 +55,78 @@ module.exports.getNotes = function (req, res) {
         Note.find({ user: userId }, function (err, notes) {
             return res.status(200).json(notes);
         })
+    } catch (err) {
+        console.log('Error ', err);
+        return res.status(500).json({ 'error': 'Internal Server Error' });
+    }
+}
+
+module.exports.updateNote = async function (req, res) {
+    try {
+        // destructuring req.body
+        const { title, description, tag } = req.body;
+
+        // create a update object
+        const update = {};
+        if (title) { update.title = title }
+        if (description) { update.description = description }
+        if (tag) { update.tag = tag }
+
+        // find the note
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).json({
+                message: "Note doesn't found"
+            });
+        }
+
+        // get user id through the payload
+        const jwt_payload = jwt.decode(req.body.token);
+        const userId = jwt_payload;
+
+        // check weather the note belong to the user
+        if (note.user == userId) {
+            let updateNote = await Note.findByIdAndUpdate(req.params.id, update);
+            return res.status(200).json({
+                message: 'Note updated successfully',
+                data: update
+            });
+        } else {
+            return res.status(401).json({
+                message: "Sorry you're not allowed to update this post, this note doesn't belong to you."
+            });
+        }
+    } catch (err) {
+        console.log('Error ', err);
+        return res.status(500).json({ 'error': 'Internal Server Error' });
+    }
+}
+
+module.exports.delete = async function (req, res) {
+    try {
+        // find the node
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).json({
+                message: "Note doesn't found"
+            });
+        }
+
+        // get user id through the payload
+        const jwt_payload = jwt.decode(req.body.token);
+        const userId = jwt_payload;
+
+        // check weather the note belong to the user
+        if (note.user == userId) {
+            await Note.findByIdAndDelete(req.params.id);
+            return res.status(200).json({
+                message: 'Note deleted successfully',
+            });
+        } else {
+            return res.status(401).json({
+                message: "Sorry you're not allowed to delete this post, this note doesn't belong to you."
+            });
+        }
     } catch (err) {
         console.log('Error ', err);
         return res.status(500).json({ 'error': 'Internal Server Error' });
--- routes/notes.js
+++ routes/notes.js
@@ -5,6 +5,7 @@ const notesController = require('../controllers/notes_controller');
 router.post('/create', notesController.createNote);
 router.get('/fetch', notesController.getNotes);
 router.put('/update/:id', notesController.updateNote);
+router.delete('/delete/:id', notesController.delete);
 
 
 module.exports = router;
